5dbb887b06032a0422bbf6cf37310f94adce9987
CharlesSchimmel <charlesschimmel@gmail.com>
2018-09-30 17:03:19 -0500

    added some utils

--- src/KodiRPC/Util.hs
+++ src/KodiRPC/Util.hs
@@ -3,6 +3,8 @@ module KodiRPC.Util where
 import Data.HashMap.Lazy as HM
 import Data.Aeson
 import Control.Monad
+import Data.Text
+import Data.Scientific
 
 -- Aeson convenience methods
 
@@ -11,7 +13,6 @@ lookup' :: Text -> Value -> Maybe Value
 lookup' key (Object map) = HM.lookup key map
 lookup' _ _ = Nothing
 
-(#?) :: HashMap k v -> k -> Maybe v
 map #? key = HM.lookup key map
 
 (>#?) :: Maybe Value -> Text -> Maybe Value
@@ -23,5 +24,12 @@ maybeStr _ = Nothing
 maybeNum (Just (Number n)) = Just n
 maybeNum _ = Nothing
 
+maybeBool (Just (Bool b)) = Just b
+maybeBool _ = Nothing
+
 maybeSci :: Scientific -> Maybe Float
 maybeSci s = either Just (const Nothing) $ floatingOrInteger s
+
+maybeInt :: Scientific -> Maybe Int
+maybeInt s = either (const Nothing) Just $ floatingOrInteger s
+
